442841f61595e9081c29fa1daf7a61c5daab2c39
Shom-nath <nath.shom@gmail.com>
2024-01-06 22:04:07 -0500

    Create Bear.java
    new file

---
+++ Bear.java
@@ -0,0 +1,16 @@
+package HelloWorld;
+
+public class Bear {
+    String species;
+    public Bear(String speciesOfBear) {
+        species = speciesOfBear;
+    }
+
+    public static void main(String[] args) {
+        Bear baloo = new Bear("Sloth bear");
+        System.out.println(baloo); // Prints: Bear@4517d9a3
+        // set object to null
+        baloo = null;
+        System.out.println(baloo); // Prints: null
+    }
+}
